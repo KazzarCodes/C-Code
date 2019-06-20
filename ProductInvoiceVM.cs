e65d94c7f9397d9420f8aac03cc02beb6ec2ba99
KazzarCodes <49265895+KazzarCodes@users.noreply.github.com>
2019-06-20 11:09:22 +0200

    Create  ProductInvoiceVM.cs

---
+++ ProductInvoiceVM.cs
@@ -0,0 +1,32 @@
+public class ProductInvoiceVM : PersistableViewModelBase<VInvoice>
+{
+    protected override ICommandModel GetCommandModel()
+    {
+        var lineItems = new List<InvoiceItemModel>();
+
+        foreach (var item in ItemModels.Where(im => im.IncludeInInvoice))
+        {
+            var mappedItem = AutoMapper.MapObject(item, new InvoiceItemModel());
+            mappedItem.Units = (decimal)item.PickedUnits;
+            lineItems.Add(mappedItem);
+        }
+
+        var commandModel = new CreateInvoiceCommandModel()
+        {
+            SalesOrderId = SalesOrderId,
+            PickTicketId = PickTicketId,
+            CustomerId = CustomerId,
+            SubCustomerId = SubCustomerId,
+            InvoiceDate = InvoicedDate,
+            Comment = Comment,
+            Reference = Reference,
+            AddressDetailId = DeliveryAddressDetailId,
+            LinkedJobId = 0,
+            TaxTypeId = TaxTypeId,
+            TermId = TermId,
+            InvoiceItems = lineItems
+        };
+
+        return commandModel;
+    }
+}
